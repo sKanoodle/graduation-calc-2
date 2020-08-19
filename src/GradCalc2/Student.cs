5344fbd2d403aeac896530d71545ccfd2149c498
sKanoodle <sKanoodle@users.noreply.github.com>
2020-08-20 00:32:40 +0200

    save original student to reset changes

--- src/GradCalc2/Student.cs
+++ src/GradCalc2/Student.cs
@@ -9,15 +9,25 @@ namespace GradCalc2
     {
         public string FirstName { get; }
         public string LastName { get; }
-        public Grade[] Grades { get; }
+        private readonly Grade[] _Grades;
+        public IEnumerable<Grade> Grades => _Grades;
 
         public Student(string firstName, string lastName, Grade[] grades)
         {
             FirstName = firstName;
             LastName = lastName;
-            Grades = grades;
+            _Grades = grades;
         }
 
         public Grade this[Subject subject] => Grades.FirstOrDefault(g => g.Subject == subject);
+
+        public Student WithGrade(Grade grade)
+        {
+            var result = new Student(FirstName, LastName, _Grades);
+            for (int i = 0; i < result._Grades.Length; i++)
+                if (_Grades[i].Subject == grade.Subject)
+                    _Grades[i] = grade;
+            return result;
+        }
     }
 }
